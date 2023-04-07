e40fe5d5667b4426c4ad0bb8eaa9fdaca39d84eb
Mert Demir <101717064+mertdemirtez@users.noreply.github.com>
2023-04-07 18:00:18 +0300

    Add files via upload

---
+++ KelimeTahminOyunu/KelimeTahminOyunu/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace KelimeTahminOyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.answerTextBox = new System.Windows.Forms.TextBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.wordTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // answerTextBox
+            // 
+            this.answerTextBox.Location = new System.Drawing.Point(165, 181);
+            this.answerTextBox.Name = "answerTextBox";
+            this.answerTextBox.Size = new System.Drawing.Size(100, 20);
+            this.answerTextBox.TabIndex = 0;
+            // 
+            // wordTextBox
+            // 
+            this.wordTextBox.Location = new System.Drawing.Point(165, 242);
+            this.wordTextBox.Name = "wordTextBox";
+            this.wordTextBox.Size = new System.Drawing.Size(100, 20);
+            this.wordTextBox.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.wordTextBox);
+            this.Controls.Add(this.answerTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox answerTextBox;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.TextBox wordTextBox;
+    }
+}
+
---
+++ KelimeTahminOyunu/KelimeTahminOyunu/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System.Windows.Forms;
+using System;
+
+public partial class Form1 : Form
+{
+    private string[] words = { "kalem", "defter", "kitap", "kırtasiye", "matematik", "fizik", "kimya", "tarih" };
+    private string currentWord;
+    private int remainingTime = 30;
+
+    public Form1()
+    {
+        InitializeComponent();
+        StartNewGame();
+    }
+
+    private void StartNewGame()
+    {
+        remainingTime = 30;
+        currentWord = words[new Random().Next(words.Length)];
+        wordTextBox.Text = currentWord;
+        timerLabel.Text = remainingTime.ToString();
+        timer1.Start();
+    }
+
+    private void CheckAnswer()
+    {
+        if (answerTextBox.Text.ToLower() == currentWord.ToLower())
+        {
+            MessageBox.Show("Tebrikler, doğru tahmin!");
+            StartNewGame();
+        }
+        else
+        {
+            MessageBox.Show("Yanlış tahmin, tekrar dene!");
+            answerTextBox.Text = "";
+        }
+    }
+
+    private void timer1_Tick(object sender, EventArgs e)
+    {
+        remainingTime--;
+        timerLabel.Text = remainingTime.ToString();
+        if (remainingTime == 0)
+        {
+            timer1.Stop();
+            MessageBox.Show($"Süre doldu! Cevap: {currentWord}");
+            StartNewGame();
+        }
+    }
+
+    private void guessButton_Click(object sender, EventArgs e)
+    {
+        CheckAnswer();
+    }
+
+    private void nextButton_Click(object sender, EventArgs e)
+    {
+        StartNewGame();
+    }
+}
